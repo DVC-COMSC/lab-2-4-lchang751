3825ccc84a263218a1dbf2c5da32f9b02428ade4
lchang751 <136843170+lchang751@users.noreply.github.com>
2023-06-24 21:22:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     ##################################################
     original_str="Python Programming"
     sub1=original_str[:6]
-    sub2=original_str[:-11]
+    sub2=original_str[7:]
     merged_str=sub2+sub1
 
     print(sub1)
